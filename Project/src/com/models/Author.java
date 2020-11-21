6bc85266cbda5649b19f9fbab939be69f39d4e8d
achraf.fakhfakh <achraf.fakhfakh@esprit.tn>
2020-11-21 15:02:42 +0100

    add author model

--- Project/src/com/models/Author.java
+++ Project/src/com/models/Author.java
@@ -10,5 +10,49 @@ package com.models;
  * @author ASUS
  */
 public class Author {
+    private int id ;
+    private String name ;
+    private String description;
+    private String PloUrl;
+
+    public Author(int id, String name, String description, String PloUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.PloUrl = PloUrl;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPloUrl(String PloUrl) {
+        this.PloUrl = PloUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPloUrl() {
+        return PloUrl;
+    }
+    
     
 }
